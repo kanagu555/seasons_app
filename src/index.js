96a2c072a4dcb00213922709a6767f5c813797f1
Kanagaraj <kanagarajwhb@gmail.com>
2021-11-12 23:06:34 +0530

    implemented life cycle mentioned and default props

--- src/index.js
+++ src/index.js
@@ -1,21 +1,35 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Spinner from "./Spinner";
+import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { lat: null };
+  state = { lat: null, errorMessage: "" };
 
+  componentDidMount() {
     window.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        this.setState({ lat: position.coords.latitude });
-      },
-      (err) => console.log(err)
+      (position) => this.setState({ lat: position.coords.latitude }),
+      (err) => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  renderContent() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+    return (
+      <div>
+        <Spinner message="Please accept location request" />
+      </div>
     );
   }
 
   render() {
-    return <div>Latitude: {this.state.lat}</div>;
+    return <div>{this.renderContent()}</div>;
   }
 }
 
